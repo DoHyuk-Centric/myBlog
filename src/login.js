8e68c601018fb9d3f6d6cc15d282de65a421df4f
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-01-20 10:59:09 +0900

    github 배포 TEST
    - CDN 제거

--- src/login.js
+++ src/login.js
@@ -6,32 +6,38 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-const provider = new GithubAuthProvider();
+loginHandler();
 
-document.getElementById("githubLogin").addEventListener("click", () => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log("GitHub 로그인 성공", result.user);
-    })
-    .catch((error) => {
-      console.error(error.code, error.message);
+function loginHandler() {
+    
+    const provider = new GithubAuthProvider();
+    
+    document.getElementById("githubLogin").addEventListener("click", () => {
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          console.log("GitHub 로그인 성공", result.user);
+        })
+        .catch((error) => {
+          console.error(error.code, error.message);
+        });
     });
-});
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("로그인 유지", user.email);
-  } else {
-    console.log("로그아웃 상태");
-  }
-});
-
-document.getElementById("googleLogin").addEventListener("click", () => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log("Google 로그인 성공", result.user);
-    })
-    .catch((error) => {
-      console.error(error.code, error.message);
+    
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("로그인 유지", user.email);
+      } else {
+        console.log("로그아웃 상태");
+      }
     });
-});
+    
+    document.getElementById("googleLogin").addEventListener("click", () => {
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          console.log("Google 로그인 성공", result.user);
+        })
+        .catch((error) => {
+          console.error(error.code, error.message);
+        });
+    });
+
+}
