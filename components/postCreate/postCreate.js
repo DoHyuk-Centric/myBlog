14efaca418339ac562190ade969ce8ef8cf930df
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-02-02 00:03:13 +0900

    ➕ 게시글 DB 연동(비동기식 처리) - postCreate에서 게시글이 온전히 DB로 소통이 만료되었을때 devlog.html로 이동

---
+++ components/postCreate/postCreate.js
@@ -0,0 +1,31 @@
+import { supabase } from "../../src/supabase.js";
+
+
+function postCreate() {
+    const title = document.getElementById("title");
+    const content = document.getElementById("content");
+    const createPostTable = document.getElementById("createPostTable");
+    const temp_createPost = document.getElementById("temp_createPost");
+    
+    createPostTable.addEventListener("click", () => {
+        creatPostTable(title.value, content.value);
+    });
+}
+
+export async function creatPostTable(title, content) {
+    const { data, error } = await supabase.from("Posts").insert([
+        {
+            title: title,
+            content: content,
+        },
+    ]);
+    
+    if (error) {
+        console.log("게시글 저장 실패", err);
+        return;
+    }
+    
+    window.location.href = "/pages/devlog.html";
+}
+
+postCreate();
