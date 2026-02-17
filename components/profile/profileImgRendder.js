7395ed02e6f279d4cb8426d56b47ad2209035229
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-02-17 12:23:22 +0900

    ✏️ profile: 프로필 이미지 저장 및 불러오기

--- components/profile/profileImgRendder.js
+++ components/profile/profileImgRendder.js
@@ -1,7 +1,33 @@
-function profileImgRendder(event) {
-  const reader = new FileReader();
-  reader.onload = function (e) {
-    document.getElementById("profile-image").src = reader.result;
-  };
-  reader.readAsDataURL(event.target.files[0]);
+import { supabase } from "../../src/supabase";
+
+profileImgRendder();
+
+async function profileImgRendder() {
+  const profileImg = document.getElementById("profile-img");
+  const profileImage = document.getElementById("profile-image");
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const {
+    data: { publicUrl },
+  } = supabase.storage.from("profile").getPublicUrl(`${user.id}/profile.jpg`);
+  profileImage.src = publicUrl;
+
+  profileImg.addEventListener("change", async () => {
+    const file = profileImg.files[0];
+    if (!file) {
+      console.error("파일이 없습니다.");
+      return;
+    }
+    const { data, error } = await supabase.storage
+      .from("profile")
+      .upload(`${user.id}/profile.jpg`, file, {
+        cacheControl: "3600",
+        upsert: true,
+      });
+
+    if (error) {
+      console.error("이미지파일 업로드 실패", error);
+    }
+  });
 }
