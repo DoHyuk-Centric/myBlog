c14fe67580efd9b9be074d5b64ae9e3a96e07ae4
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-02-18 00:11:45 +0900

    ✏️profile: 생년월일 입력값 자동완성 - 기존보다 조금 더 자연스럽게 변환 - YYYY를 쳤을경우 뒤에 바로 - 가 기입됨, YYYY-MM까지 칠경우 또 뒤에 바로 -가 기입되도록 구현

--- components/profile/profileInnerBirth.js
+++ components/profile/profileInnerBirth.js
@@ -2,15 +2,15 @@ function profileInnerBirth() {
     const birth = document.getElementById("profileBirth");
     
     birth.addEventListener("input", () => {
-        let value = birth.value.replace(/\D/g, ""); // 숫자 이외의 문자 제거
+        let value = birth.value.replace(/\D/g, "");
         if (value.length > 8) {
-            value = value.slice(0, 8); // 최대 8자리로 제한
+            value = value.slice(0, 8);
         }
-        if(value.length >= 4 && value.length < 5) {
-            value = value.replace(/(\d{4})(\d{2})?/, "$1-$2"); // YYYY-MM 형식으로 변환
+        if(value.length >= 4 && value.length < 6) {
+            value = value.replace(/(\d{4})?/, "$1-");
         }
-        else if (value.length >= 5) {
-            value = value.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3"); // YYYY-MM-DD 형식으로 변환
+        else if (value.length >= 6) {
+            value = value.replace(/(\d{4})(\d{2})/, "$1-$2-");
         }
         birth.value = value;
     });
