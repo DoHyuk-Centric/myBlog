b72f7ca26d68678986eb73a6ce88f2d36e5599c5
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-03-14 00:13:26 +0900

    feat: Devlog페이지 모바일 글쓰기 버튼 추가 - 로그인/로그아웃시에 따라 글쓰기 버튼을 hidden, block

--- components/devLog/login/devLoginCheck.js
+++ components/devLog/login/devLoginCheck.js
@@ -3,6 +3,7 @@ devLoginCheck();
 function devLoginCheck() {
   const DevLogActionSignedIn = document.getElementById("DevLogActionSignedIn");
   const DevLogActionSignedOut = document.getElementById("DevLogActionSignedOut");
+  const mobileWriteButton = document.getElementById("mobileWriteButton");
   const userProfileState = {
     img: document.getElementById("profileImg"),
     nickName: document.getElementById("profileNickName"),
@@ -17,11 +18,13 @@ function devLoginCheck() {
     })
     DevLogActionSignedOut.classList.remove("hidden");
     DevLogActionSignedIn.classList.add("hidden");
+    mobileWriteButton.classList.add("hidden");
   } else {
     Object.values(userProfileState).forEach((profile) => {
       profile.classList.remove("hidden");
     })
     DevLogActionSignedIn.classList.remove("hidden");
+    mobileWriteButton.classList.remove("hidden");
     DevLogActionSignedOut.classList.add("hidden");
   }
 }
